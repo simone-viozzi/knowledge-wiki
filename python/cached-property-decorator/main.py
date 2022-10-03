aeb87e3cd68b3a4ae2711dd85f9676affaf07e92
simone-viozzi <viozzis333@gmail.com>
2022-10-03 15:38:06 +0200

    this doesn't really make things faster, but I tried

--- python/cached-property-decorator/main.py
+++ python/cached-property-decorator/main.py
@@ -16,16 +16,17 @@ from time import sleep, perf_counter
 
 def cached_property(func):
     """decorator used to cache expensive object attribute lookup"""
-    
-    def wrapper(self):
-        """wrapper function"""
-        if not hasattr(self, "_cache"):
-            self._cache = {}
-        if func.__name__ not in self._cache:
-            self._cache[func.__name__] = func(self)
-        return self._cache[func.__name__]
+    name = f'_cached_{func.__name__}'
 
-    return wrapper
+    @property
+    def lookup(instance):
+        if hasattr(instance, name):
+            return getattr(instance, name)
+        value = func(instance)
+        setattr(instance, name, value)
+        return value
+
+    return lookup
 
 
 class Planet:
@@ -33,30 +34,23 @@ class Planet:
 
     GRAVITY_CONSTANT = 42
     TEMPORAL_SHIFT = 0.12345
-    SOLAR_MASS_UNITS = "M\N{SUN}"
+    SOLAR_MASS_UNITS = 'M\N{SUN}'
 
     def __init__(self, color):
         self.color = color
-        self._mass = None
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({repr(self.color)})"
+        return f'{self.__class__.__name__}({repr(self.color)})'
 
-    @property
     @cached_property
     def mass(self):
         scale_factor = random()
         sleep(self.TEMPORAL_SHIFT)
-        self._mass = (
-            f"{round(scale_factor * self.GRAVITY_CONSTANT, 4)} "
-            f"{self.SOLAR_MASS_UNITS}"
-        )
-        return self._mass
-    
-    @mass.setter
-    def mass(self, _):
-        raise AttributeError("can't set attribute")
+        return (f'{round(scale_factor * self.GRAVITY_CONSTANT, 4)} '
+                f'{self.SOLAR_MASS_UNITS}')
+                
 
+import contextlib
 if __name__ == "__main__":
     blue = Planet('blue')
 
@@ -66,3 +60,6 @@ if __name__ == "__main__":
     end_time = perf_counter()
     elapsed_time = end_time - start_time
     assert elapsed_time < .5
+
+    with contextlib.suppress(AttributeError):
+        blue.mass = 42

12589f7bbf54859dcbe7acbc87e67a21045d7e98
simone-viozzi <viozzis333@gmail.com>
2022-10-03 18:36:04 +0200

    Code was clean until manager requested to fuck it up

--- python/determine-the-strength-of-a-password/main.py
+++ python/determine-the-strength-of-a-password/main.py
@@ -7,26 +7,33 @@ def password_complexity(password):  # sourcery skip: hoist-repeated-if-condition
     2. Password contains one or more numbers in addition to one or more characters,
     3. Password has one or more special characters,
     4. Password has a minimum length of 8 characters,
-    5. Password starting 8 chars ("long enough") that doesn't have repeating characters 
+    5. Password starting 8 chars ("long enough") that doesn't have repeating characters
         next to each other ('1234abcbd' = good, '1234abbd' = bad, because it has a repeated b)
-       
+
        return: score int"""
-    
+
     score = 0
 
-    if re.search(r'[A-Z]+', password) and re.search(r'[a-z]+', password):
+    if re.search(r"[A-Z]+", password) and re.search(r"[a-z]+", password):
         score += 1
 
-    if re.search(r'[a-z]+', password.lower()) and re.search(r'\d+', password):
+    if re.search(r"[a-z]+", password.lower()) and re.search(r"\d+", password):
         score += 1
 
-    if re.search('[^A-Za-z0-9]+', password):
+    if re.search("[^A-Za-z0-9]+", password):
         score += 1
 
     if len(password) >= 8:
         score += 1
 
-    if len(password) >= 8 and not re.search(r'(.)(\1{1,})', password):
+    # (.)(\1{1,}):
+    #   - . = any character
+    #   - (.) = capture group 1
+    #   - \1 = first capture group
+    #   - {1,} = 1 or more times
+    #   - \1{1,} = the previous capture group 1 or more times (greedy)
+    #   - (\1{1,}) = a capture group of the previous capture group repeated 1 or more times (greedy)
+    if len(password) >= 8 and not re.search(r"(.)(\1{1,})", password):
         score += 1
 
     return score
